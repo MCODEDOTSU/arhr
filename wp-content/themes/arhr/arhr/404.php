253b4112546be8ae09d39ddd0287e93987459369
Astrobs.ru <info@mcode.su>
2020-08-05 15:11:59 +0300

    images

---
+++ wp-content/themes/arhr/arhr/404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+    <div class="single-404 page-content fix">
+
+        <h1>Ой! Страница не найдена.</h1>
+
+        <p>По данному адресу ничего не найдено. Попробуйте воспользоваться поиском.</p>
+
+    </div>
+
+    <div class="searchpage page-content fix">
+
+        <?php get_search_form(); ?>
+
+    </div>
+
+<?php get_footer(); ?>
